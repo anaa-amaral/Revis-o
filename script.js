d663fdb2bd812c300b25da7a462d1b1c62769b31
ana_l_amaral <ana_l_amaral@estudante.sesisenai.org.br>
2025-04-04 09:54:35 -0300

    Add questão 3

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let soma = 0
     }
     while ( vezes > n1);*/
 
-//Questão 2
+/*Questão 2
 let intervalo = null;
 
 var timer = function(){
@@ -30,5 +30,58 @@ var stop = function(){
     }
 
 timer();
-stop();
+stop();*/
 
+//Questão 3
+let ja = parseFloat(prompt('Digite o seu total de ganhos do mês de Janeiro:'));
+let f = parseFloat(prompt('Digite o seu total de ganhos do mês de Fevereiro :'));
+let m = parseFloat(prompt('Digite o seu total de ganhos do mês de Março:'));
+let a = parseFloat(prompt('Digite o seu total de ganhos do mês de Abril:'));
+let ma = parseFloat(prompt('Digite o seu total de ganhos do mês de Maio:'));
+let ju = parseFloat(prompt('Digite o seu total de ganhos do mês de Junho:'));
+let j = parseFloat(prompt('Digite o seu total de ganhos do mês de Julho:'));
+let ag = parseFloat(prompt('Digite o seu total de ganhos do mês de Agosto:'));
+let s = parseFloat(prompt('Digite o seu total de ganhos do mês de Setembro:'));
+let o = parseFloat(prompt('Digite o seu total de ganhos do mês de Outubro:'));
+let n = parseFloat(prompt('Digite o seu total de ganhos do mês de Novembro:'));
+let d = parseFloat(prompt('Digite o seu total de ganhos do mês de Dezembro:'));
+
+let soma = 0
+
+
+    soma = ja+f+m+a+ma+ju+j+ag+s+o+n+d
+    console.log ( 'Seu total de Ganhos do Ano foi de:' );
+    console.log ( soma );
+
+let ja1 = parseFloat(prompt('Digite o seu total de gastos do mês de Janeiro:'));
+let f1 = parseFloat(prompt('Digite o seu total de gastos do mês de Fevereiro :'));
+let m1 = parseFloat(prompt('Digite o seu total de gastos do mês de Março:'));
+let a1 = parseFloat(prompt('Digite o seu total de gastos do mês de Abril:'));
+let ma1 = parseFloat(prompt('Digite o seu total de gastos do mês de Maio:'));
+let ju1 = parseFloat(prompt('Digite o seu total de gastos do mês de Junho:'));
+let j1 = parseFloat(prompt('Digite o seu total de gastos do mês de Julho:'));
+let ag1 = parseFloat(prompt('Digite o seu total de gastos do mês de Agosto:'));
+let s1 = parseFloat(prompt('Digite o seu total de gastos do mês de Setembro:'));
+let o1 = parseFloat(prompt('Digite o seu total de gastos do mês de Outubro:'));
+let n1 = parseFloat(prompt('Digite o seu total de gastos do mês de Novembro:'));
+let d1 = parseFloat(prompt('Digite o seu total de gastos do mês de Dezembro:'));
+
+let somas = 0
+
+
+    somas = ja1+f1+m1+a1+ma1+ju1+j1+ag1+s1+o1+n1+d1
+    console.log ( 'Seu total de Gastos do Ano foi de:' );
+    console.log ( somas );
+
+let saldo = 0
+
+    saldo = soma --- somas
+    console.log ( 'Seu total de Saldo do Ano foi de:' );
+    console.log ( saldo );
+
+   if (saldo > 0){ ('Você teve lucro')
+   }else if (saldo === 0){
+    console.log ( 'Você não ganhou nada')
+   } else{
+    console.log ( 'Você teve prejuízo')
+   } 
