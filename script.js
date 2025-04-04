b9f384bccbe354e9cf571a5ffca4beee2f660467
ana_l_amaral <ana_l_amaral@estudante.sesisenai.org.br>
2025-04-04 09:32:58 -0300

    Add questão 2

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-//Questão 1
+/*Questão 1
 let n1 = 0
 let n2 = 0
 let soma = 0
-let vezes = parseFloat(prompt('Digite quantos números voc~e deseja somar:'));
+//let vezes = parseFloat(prompt('Digite quantos números voc~e deseja somar:'));
 
     do{
        let n2 = parseFloat(prompt('Digite seu número:'));
@@ -10,4 +10,25 @@ let vezes = parseFloat(prompt('Digite quantos números voc~e deseja somar:'));
        console.log ( soma + n2 )
        n1 = n1 + n2
     }
-    while ( vezes > n1);
+    while ( vezes > n1);*/
+
+//Questão 2
+let intervalo = null;
+
+var timer = function(){
+    let count = 10;
+    intervalo = setInterval(() =>{
+        console.log(count);
+        count--
+        }, 1000)}
+
+var stop = function(){
+    setTimeout(() => {
+        clearInterval(intervalo)
+        console.log('Lançamento');        
+            }, 10000)
+    }
+
+timer();
+stop();
+
